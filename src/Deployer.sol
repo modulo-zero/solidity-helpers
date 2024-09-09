4e2116c5960930a24ef948901a90d2de397c7ddd
modulo0 <nick@modulozero.xyz>
2024-09-09 12:51:54 -0700

    Remove

--- src/Deployer.sol
+++ src/Deployer.sol
@@ -556,57 +556,6 @@ abstract contract Deployer is Script {
         vm.stopBroadcast();
     }
 
-    function computeAddress(address addr, uint64 offset) public view returns (address) {
-        uint64 nonce = vm.getNonce(addr) + offset;
-        bytes memory data;
-        bytes1 len = bytes1(0x94);
-
-        /** 
-         * @dev The integer zero is treated as an empty byte string and therefore has only one
-         * length prefix, 0x80, which is calculated via 0x80 + 0.
-         */
-        if (nonce == 0x00) data = abi.encodePacked(bytes1(0xd6), len, addr, bytes1(0x80));
-
-        /** 
-         * @dev A one-byte integer in the [0x00, 0x7f] range uses its own value as a length prefix,
-         * there is no additional "0x80 + length" prefix that precedes it.
-         */
-        else if (nonce <= 0x7f) data = abi.encodePacked(bytes1(0xd6), len, addr, uint8(nonce));
-
-        /**
-         * @dev In the case of `nonce > 0x7f` and `nonce <= type(uint8).max`, we have the following
-         * encoding scheme (the same calculation can be carried over for higher nonce bytes):
-         * 0xda = 0xc0 (short RLP prefix) + 0x1a (= the bytes length of: 0x94 + address + 0x84 + nonce, in hex),
-         * 0x94 = 0x80 + 0x14 (= the bytes length of an address, 20 bytes, in hex),
-         * 0x84 = 0x80 + 0x04 (= the bytes length of the nonce, 4 bytes, in hex).
-         */
-        else if (nonce <= type(uint8).max) data = abi.encodePacked(bytes1(0xd7), len, addr, bytes1(0x81), uint8(nonce));
-        else if (nonce <= type(uint16).max) data = abi.encodePacked(bytes1(0xd8), len, addr, bytes1(0x82), uint16(nonce));
-        else if (nonce <= type(uint24).max) data = abi.encodePacked(bytes1(0xd9), len, addr, bytes1(0x83), uint24(nonce));
-        else if (nonce <= type(uint32).max) data = abi.encodePacked(bytes1(0xda), len, addr, bytes1(0x84), uint32(nonce));
-        else if (nonce <= type(uint40).max) data = abi.encodePacked(bytes1(0xdb), len, addr, bytes1(0x85), uint40(nonce));
-        else if (nonce <= type(uint48).max) data = abi.encodePacked(bytes1(0xdc), len, addr, bytes1(0x86), uint48(nonce));
-        else if (nonce <= type(uint56).max) data = abi.encodePacked(bytes1(0xdd), len, addr, bytes1(0x87), uint56(nonce));
-        else data = abi.encodePacked(bytes1(0xde), len, addr, bytes1(0x88), uint64(nonce));
-
-        return address(uint160(uint256(keccak256(data))));
-    }
-
-    function computeCreate2Address(bytes32 salt, bytes32 creationCodeHash) public view returns (address addr) {
-        address contractAddress = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
-
-        assembly {
-            let ptr := mload(0x40)
-
-            mstore(add(ptr, 0x40), creationCodeHash)
-            mstore(add(ptr, 0x20), salt)
-            mstore(ptr, contractAddress)
-            let start := add(ptr, 0x0b)
-            mstore8(start, 0xff)
-            addr := keccak256(start, 85)
-        }
-    }
-
     function _getEnvironment() internal view returns (string memory url, string memory layer, uint32 chainid) {
         string memory res = vm.readFile(string.concat(vm.envOr("PROJECT_DIR", string("")), "/mod.config.json"));
         (string memory env, string memory _layer) = _getDeploymentContext();
